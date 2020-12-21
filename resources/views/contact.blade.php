c1b3769d0e57e7c603ebbccacb9024b855f5e32e
ask.rj10@gmail.com <ask.rj10@gmail.com>
2020-12-21 16:19:47 -0300

    subindo sistema

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+
+@section('title', 'HDC Events')
+
+
+@section('content')
+
+<h1>este á pagina de contato</h1>
+
+
+@endsection 
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.main')
+
+@section('title', 'HDC Events')
+
+@section('content')
+
+<div id="search-container" class="col-md-12">
+    <form action="/" method="GET">
+        <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
+    </form>
+</div>
+<div id="events-container" class="col-md-12">
+    @if ($search)
+    <h1>Buscando por {{ $search }}</h1>
+    @else
+    <h2>Próximos Eventos</h2>
+    <p class="subtitle">Veja os eventos dos próximos dias</p>
+    @endif
+    <div id="cards-container" class="row">
+        @foreach($events as $event)
+        <div class="card col-md-3">
+            <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}">
+            <div class="card-body">
+                <p class="card-date">{{ date('d/m/Y', strtotime($event->date )) }}</p>
+                <h5 class="card-title">{{ $event->title }}</h5>
+                <p class="card-participants">X Participantes</p>
+                <a href="/events/{{ $event-> id }}" class="btn btn-primary">Saber mais</a>
+            </div>
+        </div>
+        @endforeach
+        @if (count($events) ==0 && $search)
+        <p>Não foi possivel encontrar nenhum evento com {{ $search }}! <a href="/">Ver todos os eventos</a> </p>
+        @elseif(count($events) == 0)
+        <p>Não há eventos cadastrados</p>
+        @endif
+    </div>
+</div>
+
+@endsection
